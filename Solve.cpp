25bd7e3e951ae00762116d9b6ba0742f0d3d8bc0
Alex <sasha-yarushin@mail.ru>
2020-09-01 00:09:35 +0500

    ---

--- Solve.cpp
+++ Solve.cpp
@@ -5,6 +5,9 @@
 #include <stack>
 #include <map>
 #include <functional>
+#include <cmath>
+#include <cstring>
+#include <iostream>
 
 using namespace std;
 
@@ -12,8 +15,28 @@ static int originPrime[] = {2,3,5,7,11,13,17,19,23,29};
 
 struct Node{
     Node(std::string _formula) : formula(_formula), error(0) {};
-    void calc(){
 
+    std::string StringReplacer(const std::string& inputStr, const std::string& src, const std::string& dst)
+    {
+        std::string result(inputStr);
+
+        size_t pos = result.find(src);
+        while(pos != std::string::npos) {
+            result.replace(pos, src.size(), dst);
+            pos = result.find(src, pos);
+        }
+
+        return result;
+    }
+
+    void calc(){
+        int N = sizeof(originPrime) / sizeof(originPrime[0]);
+        for(int i = 0; i < N; ++i){
+            this->formula = StringReplacer(this->formula,"x",std::to_string(i+1));
+            char tmp[formula.length() + 1];
+            strcpy(tmp, formula.c_str());
+            this->error += abs(this->parse(tmp) - originPrime[i]);
+        }
     }
 
     std::string formula;
@@ -50,7 +73,11 @@ struct Solve{
 
 
     Solve(Node *_parents, int _count) : count(_count), parents(_parents){
+        for(int i = 0; i < count; ++i){
+            parents[i].calc();
+            std::cout << parents[i].error << std::endl;
 
+        }
     };
 
 };
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+
 #include "Solve.cpp"
 
 using namespace std;
